853dc114465324064059a3b4ea34ee7a572185b3
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2024-05-21 13:27:49 +0530

    Add files via upload

---
+++ BASIC/Binary_to_Decimal.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int binaryToDecimal(int binaryNum) {
+    int decimalNum = 0;
+    int i = 0;
+
+    while (binaryNum > 0) {
+        int lastDigit = binaryNum % 10;
+      
+        decimalNum=decimalNum+lastDigit*pow(2,i++);
+         binaryNum = binaryNum / 10;
+    }
+
+    return decimalNum;
+}
+
+int main() {
+    int binaryNum;
+    cout << "Enter a binary number: ";
+    cin >> binaryNum;
+
+    int decimalNum = binaryToDecimal(binaryNum);
+
+    cout << "Decimal equivalent: " << decimalNum << endl;
+
+    return 0;
+}
