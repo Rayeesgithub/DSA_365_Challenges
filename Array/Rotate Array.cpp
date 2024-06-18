ee0c22a4f7239acb81af1e5f36ec7ecd2f535475
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2024-06-18 08:01:28 +0530

    Create Rotate Array.cpp

---
+++ Array/Rotate Array.cpp
@@ -0,0 +1,21 @@
+void rotate(vector<int>& nums, int k) {
+    int n = nums.size();
+    vector<int> temp(n); // Create a temporary vector to store the rotated elements
+ // Loop through the original vector
+    for (int i = 0; i < n; i++) {
+        // Calculate the new position for each element after rotation
+        // (i + k) % n ensures that the index wraps around the array
+        temp[(i + k) % n] = nums[i];
+    }
+
+    // Copy the rotated elements from the temporary vector back to the original vector
+    for (int i = 0; i < n; i++) {
+        nums[i] = temp[i];
+    }
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+    
+    rotate(nums, k);
