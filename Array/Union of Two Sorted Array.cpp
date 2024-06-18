a5ddce81a1f46000274316759bd5b6dc9fa1d444
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2024-06-18 17:52:18 +0530

    Update Union of Two Sorted Array.cpp

--- Array/Union of Two Sorted Array.cpp
+++ Array/Union of Two Sorted Array.cpp
@@ -27,7 +27,55 @@ vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
   return Union;
 }
 // Two pointer approach  Time Compleixty : O( (m+n)) && Space Complexity : O(1)
-
+vector<int> findUnion(int arr1[], int arr2[], int n, int m) {
+    int i = 0, j = 0;
+    vector<int> ans;
+    
+    // Traverse both arrays
+    while (i < n && j < m) {
+        // Avoid duplicates in arr1
+        if (i > 0 && arr1[i] == arr1[i-1]) {
+            i++;
+            continue;
+        }
+        
+        // Avoid duplicates in arr2
+        if (j > 0 && arr2[j] == arr2[j-1]) {
+            j++;
+            continue;
+        }
+        
+        if (arr1[i] < arr2[j]) {
+            ans.push_back(arr1[i]);
+            i++;
+        } else if (arr1[i] > arr2[j]) {
+            ans.push_back(arr2[j]);
+            j++;
+        } else {
+            ans.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+    }
+    
+    // If there are remaining elements in arr1
+    while (i < n) {
+        if (i > 0 && arr1[i] != arr1[i-1]) {
+            ans.push_back(arr1[i]);
+        }
+        i++;
+    }
+    
+    // If there are remaining elements in arr2
+    while (j < m) {
+        if (j > 0 && arr2[j] != arr2[j-1]) {
+            ans.push_back(arr2[j]);
+        }
+        j++;
+    }
+    
+    return ans;
+}
 int main() {
   int n = 10, m = 7;
   int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
