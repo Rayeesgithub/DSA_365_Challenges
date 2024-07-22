766b5ad017bbd78aff2da1f0c47b15686b993d9d
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2024-07-22 15:36:43 +0530

    Update Reverse Words in a String.cpp

--- String/Reverse Words in a String.cpp
+++ String/Reverse Words in a String.cpp
@@ -40,3 +40,32 @@ broute force
 
 
 //optimal approach
+  string reverseWords(string s) {
+          int left = 0; // Initialize left pointer
+    int right = s.length() - 1; // Initialize right pointer
+    
+    string temp = ""; // Temporary string to build words
+    string ans = ""; // Final result string
+    
+    // Iterate the string and keep on adding to form a word
+    // If an empty space is encountered then add the current word to the result
+    while (left <= right) {
+        char ch = s[left];
+        if (ch != ' ') {
+            temp += ch; // Build the current word
+        } else if (ch == ' ' && !temp.empty()) {
+            if (!ans.empty()) ans = temp + " " + ans; // Add the current word to the front of the result
+            else ans = temp; // For the first word, directly assign to ans
+            temp = ""; // Reset the temp string for the next word
+        }
+        left++;
+    }
+    
+    // If not an empty string then add to the result (Last word is added)
+    if (!temp.empty()) {
+        if (!ans.empty()) ans = temp + " " + ans; // Add the last word to the front of the result
+        else ans = temp; // For the first word, directly assign to ans
+    }
+    
+    return ans;    
+    }
