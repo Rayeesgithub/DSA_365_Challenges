2febf2273d320e761ad8cdfaa41c481241acd273
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2024-07-23 15:11:18 +0530

    Create check_Palindrome.cpp

---
+++ String/check_Palindrome.cpp
@@ -0,0 +1,25 @@
+// love babbar
+
+#include<iostream>
+using namespace std;
+ bool checkPalindrome(string s){
+    int left =0; int right=s.length()-1;
+    while(left<=right){
+        if(left==right){
+            return true;
+        }
+        else{
+            left++; right--;
+        }
+    }
+    return false;
+ }
+int main(){
+    string str="racecar";
+    if(checkPalindrome(str)){
+        cout<<"this is palindromic string";
+    }
+    else{
+        cout<<"this is Not Palindromic String";
+    }
+}
