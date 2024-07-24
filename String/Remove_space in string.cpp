20ed403da443dab0d2f8b2bdd54653ff036cd15c
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2024-07-24 16:31:22 +0530

    Create Remove_space in string.cpp

---
+++ String/Remove_space in string.cpp
@@ -0,0 +1,22 @@
+// love Babar
+
+// love Babbar
+
+#include<iostream>
+using namespace std;
+string RemoveSpace(string s){
+     string str;
+    for(int i=0; i<s.length();i++){
+       
+        if(s[i]!=' '){
+          str+=s[i];
+        }
+    }
+    return str;
+}
+int main(){
+    string str="My Name is Rayees Alam";
+    cout<<"Before Replace string:"<<str<<endl;
+    string ans=RemoveSpace(str);
+    cout<<"after replace string:"<<ans<<endl;
+}
