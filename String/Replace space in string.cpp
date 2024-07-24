497e1d1a25f240244d85e3469fa1f6cd47574aa2
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2024-07-24 15:02:50 +0530

    Create Replace space in string.cpp

---
+++ String/Replace space in string.cpp
@@ -0,0 +1,17 @@
+// love babbar
+#include<iostream>
+using namespace std;
+string Replace(string s){
+    for(int i=0; i<s.length();i++){
+        if(s[i]==' '){
+           s[i]='@';
+        }
+    }
+    return s;
+}
+int main(){
+    string str="My Name is Rayees Alam";
+    cout<<"Before Replace string"<<str<<endl;
+    string ans=Replace(str);
+    cout<<"after replace string:"<<ans<<endl;
+}
