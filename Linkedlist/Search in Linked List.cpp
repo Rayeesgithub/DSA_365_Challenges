2a3226b1eb6832c0b5991fabd2af37cd91ccf567
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2024-08-04 14:35:57 +0530

    Create Search in Linked List.cpp

---
+++ Linkedlist/Search in Linked List.cpp
@@ -0,0 +1,13 @@
+// srtriver sheet
+
+ bool searchKey(int n, struct Node* head, int key) {
+        // Code here
+        Node* temp=head;
+        while(temp!=NULL){
+            if(temp->data==key){
+                return true;
+            }
+            temp=temp->next;
+        }
+        return false;
+    }
