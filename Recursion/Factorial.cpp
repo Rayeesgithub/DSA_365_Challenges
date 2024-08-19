36d1b0adf4ac270ece8fd9a689d219e6db0c9ede
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2024-08-19 22:12:26 +0530

    Create Factorial.cpp

---
+++ Recursion/Factorial.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fact(int n){
+    if(n==0 || n==1) return 1;
+    int res=n*fact(n-1);
+    return res;
+}
+int main(){
+    int n;
+    cin>>n;
+    int ans=fact(n);
+    cout<<ans<<" ";
+    return 0;
+}
