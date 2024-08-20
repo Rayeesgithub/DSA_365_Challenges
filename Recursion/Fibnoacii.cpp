f0d11e9b736a2b27828886f188bb9c6d322ca1e2
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2024-08-20 19:34:59 +0530

    Update Fibnoacii.cpp

--- Recursion/Fibnoacii.cpp
+++ Recursion/Fibnoacii.cpp
@@ -2,25 +2,40 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-  void printFib(int n){
-    int a=0; int b=1; int c;
-    if(n==0){
-        cout<<a<<" ";
-        return;
-    }
-    cout<<a<<" "<<b<<" ";
-
-    for(int i=2; i<=n;i++){
-        c=a+b;
-        cout<<c<<" ";
-        a=b;
-        b=c;
-    }
-  }
+ int printFib(int n){
+  if(n<=1) return 1;
+  int ans=printFib(n-1)+printFib(n-2);
+  return ans;
+ }
 int main(){
-    int n;
-
-    cin>>n;
-   printFib(n);
-   return 0;
+  int n;
+  cin>>n;
+  
+  for(int i=0; i<n; i++){
+    cout<<printFib(i)<<" ";
+  }
 }
+// #include<bits/stdc++.h>
+// using namespace std;
+//   void printFib(int n){
+//     int a=0; int b=1; int c;
+//     if(n==0){
+//         cout<<a<<" ";
+//         return;
+//     }
+//     cout<<a<<" "<<b<<" ";
+
+//     for(int i=2; i<=n;i++){
+//         c=a+b;
+//         cout<<c<<" ";
+//         a=b;
+//         b=c;
+//     }
+//   }
+// int main(){
+//     int n;
+
+//     cin>>n;
+//    printFib(n);
+//    return 0;
+// }
