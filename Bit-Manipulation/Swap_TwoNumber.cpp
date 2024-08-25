745247bee4c9cd726855967e2166b296f975eb9b
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2024-08-25 23:18:22 +0530

    Update Swap_TwoNumber.cpp

--- Bit-Manipulation/Swap_TwoNumber.cpp
+++ Bit-Manipulation/Swap_TwoNumber.cpp
@@ -15,3 +15,24 @@ int main(){
   cout<<"After Swapping:"<<a<<" "<<b;
   return 0;
 }
+
+// approach2-> without using temporary variable
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    // Input two numbers
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Before swapping: a = " << a << ", b = " << b << endl;
+
+  a=a+b;
+  b=a-b;
+  a=a-b;
+cout<<" after swapping: a= "<<a<<" , b= "<<b<<endl;
+    return 0;
+}
+
