30c7acb823e0cb844a912ddf2fed80615079fd89
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2024-09-16 16:06:44 +0530

    Update Union_Two_Array.cpp

--- Array/Union_Two_Array.cpp
+++ Array/Union_Two_Array.cpp
@@ -23,3 +23,59 @@ int main(){
     cout<<it<<" ";
    }
 }
+
+
+//optimal approach
+#include<bits/stdc++.h>
+using namespace std;
+void uninon(int arr1[], int arr2[],int n,int m){
+   vector<int>unione;  int i=0; int j=0;
+ while (i < n && j < m) {
+        if (arr1[i] < arr2[j]) {
+            if (unione.empty() || unione.back() != arr1[i]) {
+                unione.push_back(arr1[i]);
+            }
+            i++;
+        } else if (arr1[i] > arr2[j]) {
+            if (unione.empty() || unione.back() != arr2[j]) {
+                unione.push_back(arr2[j]);
+            }
+            j++;
+        } else {  // arr1[i] == arr2[j], add any one and move both
+            if (unione.empty() || unione.back() != arr1[i]) {
+                unione.push_back(arr1[i]);
+            }
+            i++;
+            j++;
+        }
+    }
+
+ 
+  while (i < n) {
+        if (unione.back() != arr1[i]) {
+            unione.push_back(arr1[i]);
+        }
+        i++;
+    }
+
+    // Add remaining elements of arr2, if any
+    while (j < m) {
+        if (unione.back() != arr2[j]) {
+            unione.push_back(arr2[j]);
+        }
+        j++;
+    }
+
+    // Print the union of both arrays
+    for (auto val : unione) {
+        cout << val << " ";
+    }
+}
+int main(){
+    int n=10;int m=7;
+    int arr1[n]={1,2,3,4,5,6,7,8,9,10};
+     int arr2[m]={2,3,4,4,5,11,12};
+ 
+  uninon(arr1,arr2,n,m);
+ 
+}
