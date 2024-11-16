d256e04274b5a9c64d0947108512ec1ec6a06d0b
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2024-11-16 16:33:39 +0530

    Update largest_sum_with_0.cpp

--- Array/largest_sum_with_0.cpp
+++ Array/largest_sum_with_0.cpp
@@ -1,7 +1,82 @@
 //https://www.geeksforgeeks.org/problems/largest-subarray-with-0-sum
 //striver sheet 
 
-//approach1-> \
+// Broute force
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>&arr,int n){
+    int maxi_sum=INT_MIN;
+    int maxi_len=INT_MIN;
+    for(int i=0; i<n; i++){
+        int sum=0; int len=0;
+        for(int j=i;j<n; j++){
+        sum+=arr[j];
+         len=len+1;
+        }
+        if(sum==0){
+            maxi_len=max(maxi_len,len);
+            len=0;
+        }
+    }
+    return maxi_len;
+}
+
+int main()
+{
+    int n=6;
+    vector<int>arr={9, -3, 3, -1, 6, -5};
+  
+    int ans=solve(arr,n);
+    cout<<endl;
+    cout<<"maximum Length is:"<<ans<<" ";
+    return 0;
+}
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>&arr,int n){
+    int maxi_sum=INT_MIN;
+    int maxi_len=INT_MIN;
+    for(int i=0; i<n; i++){
+        int sum=0; int len=0;
+        for(int j=i;j<n; j++){
+        sum+=arr[j];
+         len=len+1;
+        }
+        if(sum==0){
+            maxi_len=max(maxi_len,len);
+            len=0;
+        }
+    }
+    return maxi_len;
+}
+
+int main()
+{
+    int n=6;
+    vector<int>arr={9, -3, 3, -1, 6, -5};
+  
+    int ans=solve(arr,n);
+    cout<<endl;
+    cout<<"maximum Length is:"<<ans<<" ";
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+//approach2-> \
 #include <vector>
 #include <unordered_map>
 using namespace std;
@@ -30,3 +105,6 @@ int maxLen(vector<int>& arr, int n) {
 
     return maxi;
 }
+
+
+
