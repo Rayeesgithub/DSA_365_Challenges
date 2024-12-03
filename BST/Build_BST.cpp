b4f40b23ff94a6b6bd863d99750ecd48be740bca
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2024-12-03 15:30:04 +0530

    Create Build_BST.cpp

---
+++ BST/Build_BST.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data){
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+ };
+ Node* insertBST(Node* root, int data){
+    if(root==NULL){
+        root=new Node(data);
+        return root;
+    }
+    if(root->data > data){
+        root->left=insertBST(root->left,data);
+    }
+    else{
+        root->right=insertBST(root->right,data);
+    }
+    return root;
+ }
+   void TakeInput(Node* &root){
+    int data;
+    cin>>data;
+    while(data!=-1){
+        root=insertBST(root,data);
+        cin>>data;
+    }
+   }
+   void LevelOrderTraversal(Node* root){
+    if(root==NULL){
+        return;
+    }
+    queue<Node*>q;
+    q.push(root);
+    while(!q.empty()){
+        Node* temp=q.front();
+        q.pop();
+        cout<<temp->data<<" ";
+        if(temp->left!=NULL){
+            q.push(temp->left);
+        }
+        if(temp->right!=NULL){
+            q.push(temp->right);
+        }
+    }
+   }
+ int main(){
+    Node* root=NULL;
+    cout<<"enter the elemnt is:";
+    TakeInput(root);
+    LevelOrderTraversal(root);
+ }
