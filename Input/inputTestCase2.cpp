d67512520abd9d80baadab6653e4cc8772fe80e4
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-01-06 22:31:47 +0530

    Create inputTestCase2.cpp

---
+++ Input/inputTestCase2.cpp
@@ -0,0 +1,14 @@
+int main(){
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0; i<n; i++){
+       cin>>arr[i];
+    }
+    sortArr(arr,n);
+    cout<<endl;
+}
+}
