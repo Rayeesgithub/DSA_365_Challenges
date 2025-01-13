30faac1e398953665d4a4b0824d15df717936757
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-01-14 02:09:01 +0530

    Update InsertAtHead.cpp

--- Linkedlist/InsertAtHead.cpp
+++ Linkedlist/InsertAtHead.cpp
@@ -1,47 +1,66 @@
-//love babbar
-
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
 class Node{
     public:
     int data;
     Node* next;
-    Node(){
+     Node(){
         this->data=0;
         this->next=NULL;
-    }
-    Node(int data){
+     }
+     Node(int data){
         this->data=data;
         this->next=NULL;
-    }
+     }
 };
-// Function to print the linked list
-void print(Node* &head){
-    Node* temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
+void InsertAtHead(Node* &head, Node* &tail,int data){
+    if(head==NULL){
+        Node* newNode=new Node(data);
+        head=newNode;
+        tail=newNode;
+        return;
+    }
+    else{
+        Node* newNode=new Node(data);
+        newNode->next=head;
+        head=newNode;
     }
 }
-void InsertAtHead(Node* &head,int data){
+// Function to print the Linked List
+void print(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << "-> ";
+        temp = temp->next;
+    }
    
-    // step1 create a node
-    Node* newNode=new Node(data);
-    // step2 newnode ko head se attach
-    newNode->next=head;
-    //step 3 head ko newnode pr wapas
-    head=newNode;
-
-
 }
 int main(){
     Node* head=NULL;
-   InsertAtHead(head,10);
-  InsertAtHead(head,20);
-  InsertAtHead(head,30);
-  InsertAtHead(head,40);
-  InsertAtHead(head,50);
-  cout<<"Printing all LinkedList"<<endl;
-  print(head);
+    Node* tail=NULL;
+    int data,choice;
+    while(true){
+        cout<<"1. Insert at Head"<<endl;
+        cout<<"2. print"<<endl;
+        cout<<"3. exit"<<endl;
+        cout<<"enter your choice: ";
+        cin>>choice;
+        switch(choice){
+            case 1: 
+            cout << "Enter data to insert at head: ";
+             cin >> data; 
+             InsertAtHead(head, tail, data);
+              break;
+
+            case 2:
+            cout<<"print Elemnt:";
+            print(head);
+            break;
+            cout<<endl;
+            case 3:
+            return 0;
+            default:
+            cout<<"your Invalid Choice:";
+        }
+    }
 }
