5c5922bb1d102368fa2f84d05ac72d42841da3e5
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-01-14 00:06:19 +0530

    Update Longest_Common Prefix.cpp

--- String/Longest_Common Prefix.cpp
+++ String/Longest_Common Prefix.cpp
@@ -1,27 +1,36 @@
 //https://leetcode.com/problems/longest-common-prefix/description/
 //striver sheet
 
-//broute force
-string longestCommonPrefix(vector<string>& strs) {
-    // Sort the array of strings.
-    sort(strs.begin(), strs.end());
-
-    int n = strs.size(); // Get the number of strings.
-    string ans = ""; // Initialize the result string for the longest common prefix.
-    string first = strs[0]; // Get the first string in the sorted list.
-    string last = strs[n - 1]; // Get the last string in the sorted list.
-
-    // Compare characters of the first and last strings up to the length of the shorter one.
-    for(int i = 0; i < min(first.size(), last.size()); i++) {
-        // If characters match, add the character to the answer.
-        if(first[i] == last[i]) {
-            ans.push_back(first[i]); // Alternatively, ans += first[i];
-        } 
-        else {
-            // If characters do not match, return the current answer as the longest common prefix.
-            return ans;
+#include<bits/stdc++.h>
+using namespace std;
+string longestCommonPrefix(vector<string>&arr){
+  sort(arr.begin(),arr.end());
+  string first=arr[0];
+  string last=arr[arr.size()-1];
+  string ans="";
+  for(int i=0; i<min(first.size(),last.size());i++){
+    if(first[i]==last[i]){
+      ans.push_back(first[i]);
+    }
+    else{
+      break;
+    }
+  }
+  return ans;
+}
+int main(){
+  int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<string> arr(n); // Initialize the vector after reading n
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
         }
-    } 
-    // Return the answer if loop completes.
-    return ans;
+
+        string ans = longestCommonPrefix(arr);
+        cout << "Longest Common Prefix: " << ans << endl;
+    }
+  
 }
