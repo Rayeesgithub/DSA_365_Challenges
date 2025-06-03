7f2ff476a3f428da3744c62c99acecaddd4197cc
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-06-03 14:26:47 +0530

    Create DoublPyrmid.cpp

---
+++ Pattern/DoublPyrmid.cpp
@@ -0,0 +1,52 @@
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * *
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+
+      
+  #include <iostream>
+using namespace std;
+
+int main() {
+    int n= 5;
+
+   
+
+    for (int row = 0; row< n; row++) {
+        // Print leading spaces
+        for (int space = 0; space < n - row - 1; space++) {
+            cout << " ";
+        }
+
+        // Print stars with space
+        for (int star = 0; star <=row; star++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+
+      
+    for (int row = 0; row< 5; row++) {
+        // Print leading spaces
+        for (int space = 0; space < row; space++) {
+            cout << " ";
+        }
+
+        // Print stars with space
+        for (int star = 0; star<5-row; star++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
