859392627abd01b7829d9ec892efe0e4c95bd989
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-06-03 13:08:48 +0530

    Create InvertedPyrmid.cpp

---
+++ Pattern/InvertedPyrmid.cpp
@@ -0,0 +1,18 @@
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+
+
+#include<bits/stdc++.h> 
+using namespace std;
+
+int main(){
+    for(int row=0; row<5; row++){         
+        for(int col=0; col<5-row; col++){    
+            cout<<"*"<<" ";            
+        }
+        cout<<endl;                     
+    }
+}
