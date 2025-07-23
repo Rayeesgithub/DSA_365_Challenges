3aa55487c6f38e8cfd165a06e6e8195677071766
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-07-23 09:24:30 +0530

    Update Maximum consecutive ones.cpp

--- Array/Maximum consecutive ones.cpp
+++ Array/Maximum consecutive ones.cpp
@@ -1,5 +1,11 @@
 //https://leetcode.com/problems/max-consecutive-ones/
-// striver sheet
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+
+
+
 //Time Complexity: O(N) space-com=0(1)
 int findMaxConsecutiveOnes(vector<int>& nums) {
     int maxi = 0;   // Initialize maxi to store the maximum count of consecutive 1s found
@@ -40,3 +46,34 @@ int main() {
 
     return 0;
 }
+
+
+
+
+Input: arr[] = [0, 0, 1, 0, 1, 0]
+Output: 2
+Explanation: The maximum number of consecutive 0’s in the array is 2 from index 0-1.
+
+    class Solution {
+  public:
+  
+   int maxConsecBits(vector<int> &arr) {
+    int count0 = 0, count1 = 0;
+    int maxi0 = 0, maxi1 = 0;
+
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == 1) {
+            count1++;
+            maxi1 = max(maxi1, count1);
+            count0 = 0;  // Reset count0 since we found 1
+        } else {
+            count0++;
+            maxi0 = max(maxi0, count0);
+            count1 = 0;  // Reset count1 since we found 0
+        }
+    }
+
+    return max(maxi0, maxi1);
+}
+
+};
