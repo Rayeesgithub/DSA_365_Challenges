aaedcec1ea7503549b28dd677605c1dc23d23c86
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-07-23 08:40:54 +0530

    Update Missing Number.cpp

--- Array/Missing Number.cpp
+++ Array/Missing Number.cpp
@@ -1,5 +1,30 @@
 //https://leetcode.com/problems/missing-number/
 //striver sheet
+
+//broute force
+int missingNumber(vector<int>& nums) {
+         sort(nums.begin(), nums.end());
+    int ans = -1;
+       // ✅ Check if the first number is not 0
+    if (nums[0] != 0) return 0;
+    for (int i = 0; i < nums.size() - 1; i++) {
+        if (nums[i + 1] != nums[i] + 1) {
+            ans = nums[i] + 1;
+            break;
+        }
+    }
+
+    // If no gap found, missing number is next after last element
+    if (ans == -1) {
+        ans = nums[nums.size() - 1] + 1;
+    }
+
+    return ans;
+    }
+
+
+
+
 // approach1
  int missingNumber(vector<int>& nums) {
          int ans; int xor1=0; int xor2=0;
