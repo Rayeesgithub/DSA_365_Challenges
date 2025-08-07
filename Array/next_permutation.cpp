6d386b03e172225b14f811b4bd27626dbd654e9e
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-08-07 12:20:27 +0530

    Update next_permutation.cpp

--- Array/next_permutation.cpp
+++ Array/next_permutation.cpp
@@ -31,3 +31,27 @@ for(int i = n - 1; i > index; i--) {
        reverse(nums.begin()+index+1,nums.end());
     }
 };
+
+
+
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+    int n = nums.size();
+    int i = n - 2;
+
+    // Step 1: Find first decreasing element from right
+    while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+
+    if (i >= 0) {
+        // Step 2: Find element just larger than nums[i]
+        int j = n - 1;
+        while (nums[j] <= nums[i]) j--;
+        swap(nums[i], nums[j]);
+    }
+
+    // Step 3: Reverse the right part
+    reverse(nums.begin() + i + 1, nums.end());
+}
+};
