7aaff5d412facd163f87293a488c11f63b2b28ba
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-10-01 21:04:57 +0530

    Update Triplet_Sum.cpp

--- Array/Triplet_Sum.cpp
+++ Array/Triplet_Sum.cpp
@@ -25,6 +25,34 @@ public:
     return ans;
         }
 
+
+
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>ans;
+        int n=nums.size();
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                for(int k=j+1; k<n; k++){
+                    if(nums[i]+nums[j]+nums[k]==0){
+                        vector<int>triplet={nums[i],nums[j],nums[k]};
+                        sort(triplet.begin(),triplet.end());
+                        if(find(ans.begin(),ans.end(),triplet)==ans.end()){
+                            ans.push_back(triplet);
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+        
+
         // Better approach
         // Using a hashset to store seen elements
         for(int i = 0; i < n; i++) {
