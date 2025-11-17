faa179e75afd993abb776722bf4a736bc8a4a6dc
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-11-17 08:35:22 +0530

    Enhance comments on character digit conversions
    Added explanations for character to integer conversion.

--- AAAA_Learning.cpp
+++ AAAA_Learning.cpp
@@ -29,3 +29,14 @@ j=1 → sum=0+arr[1]=-1 → not zero
 j=2 → sum=0+arr[2]=2 → not zero
 
 j=3 → sum=0+arr[3]=-2 → not zero
+
+
+(3) What is -'0' ?
+  num1[p1] - '0' ->  It converts the character digit into an integer.
+  char c = '5';
+int x = c - '0';  // x = 5
+
+✅  What is +'0' ?
+Meaning: It converts an integer digit  into a character .
+  int digit = 7;
+char c = digit + '0';  // c = '7'
