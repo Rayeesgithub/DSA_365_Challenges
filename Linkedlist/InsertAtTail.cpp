4f8de42894000f71d7083aea98672de909d3cae7
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-11-25 13:41:52 +0530

    Update InsertAtTail.cpp

--- Linkedlist/InsertAtTail.cpp
+++ Linkedlist/InsertAtTail.cpp
@@ -26,18 +26,21 @@ void InsertAtHead(Node* &head, Node* &tail,int data){
         head=newNode;
     }
 }
-void InsertAtTail(Node* head,Node* tail,int data){
-    if(head==NULL){
-        Node* newNode=new Node(data);
-        head=newNode;
-        tail=newNode;
+// Insert new node at the TAIL (End)
+void InsertAtTail(Node* &head, Node* &tail, int data) {
+
+    // Case 1: If Linked List is empty
+    if (head == NULL) {
+        Node* newNode = new Node(data); // Create new node
+        head = newNode;                 // Both head and tail will point to this node
+        tail = newNode;
     }
-    else{
-        Node* newNode=new Node(data);
-        tail->next=newNode;
-        tail=newNode;
+    else {
+        // Case 2: List already has nodes
+        Node* newNode = new Node(data); // Create new node
+        tail->next = newNode;           // Connect new node after current tail
+        tail = newNode;                 // Update tail to new node
     }
-}
 void print(Node* head) {
     Node* temp = head;
     while (temp != NULL) {
@@ -48,11 +51,12 @@ void print(Node* head) {
    
 }
 int main(){
-   Node* head=new Node(10);
- Node* tail=head;
-InsertAtHead(head,tail,50);
-  InsertAtHead(head,tail,40);
-  InsertAtTail(head,tail,60);
+  Node* head = new Node(10); // First node
+    Node* tail = head;         // Tail same as head initially
+
+    InsertAtHead(head, tail, 50); // 50 -> 10
+    InsertAtHead(head, tail, 40); // 40 -> 50 -> 10
+    InsertAtTail(head, tail, 60); // 40 -> 50 -> 10 -> 60
  cout<<"printing Linklist"<<endl;
  Printing(head);
 }
