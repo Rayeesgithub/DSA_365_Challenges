8c1945342ccd4e124eadaaa0ef3753cca6dd5d31
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-11-25 14:10:30 +0530

    Update FindLength_or_CountLL.cpp

--- Linkedlist/FindLength_or_CountLL.cpp
+++ Linkedlist/FindLength_or_CountLL.cpp
@@ -2,42 +2,16 @@
 https://www.geeksforgeeks.org/problems/count-nodes-of-linked-list/0?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=count-nodes-of-linked-list
 
 
-#include <iostream>
-using namespace std;
-
-// Definition of the Node structure
-struct Node {
-    int data;
-    Node* next;
-
-    Node(int val) : data(val), next(nullptr) {}
-};
-
-// Function to count the number of nodes in a linked list
-int getCount(struct Node* head) {
-    int len = 0;  // Initialize a variable to store the length of the list
-    Node* temp = head;  // Start from the head of the list
-
-    // Traverse the list until the end (NULL) is reached
-    while (temp != nullptr) {
-        temp = temp->next;  // Move to the next node
-        len++;  // Increment the count for each node encountered
+int getCount(Node* head) {
+    int len = 0;        // Initialize a counter to count number of nodes
+    
+    Node* temp = head;  // Create a pointer to traverse the linked list
+    
+    // Loop until we reach the end of the list
+    while (temp != NULL) {
+        temp = temp->next; // Move to next node
+        len++;             // Increase count for each node visited
     }
-
-    return len;  // Return the total count of nodes in the list
-}
-
-int main() {
-    // Create a linked list with some nodes
-    Node* head = new Node(10);
-    head->next = new Node(20);
-    head->next->next = new Node(30);
-
-    // Get the count of nodes in the list
-    int count = getCount(head);
-
-    // Print the count
-    cout << "The number of nodes in the linked list is: " << count << endl;
-
-    return 0;
+    
+    return len;  // Return the total number of nodes counted
 }
