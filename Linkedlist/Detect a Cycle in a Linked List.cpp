39f52f1a924a669b5e3b201c92130c58b85ed0a5
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-11-26 13:23:41 +0530

    Update Detect a Cycle in a Linked List.cpp

--- Linkedlist/Detect a Cycle in a Linked List.cpp
+++ Linkedlist/Detect a Cycle in a Linked List.cpp
@@ -41,3 +41,31 @@ bool hasCycle(ListNode *head) {
         // If loop breaks without meeting → no cycle
         return false;
     }
+
+
+
+
+// 2nd approach->
+bool detectLoop(Node* head) {
+        // Initialize a pointer at head
+        Node* temp = head;
+
+        // Create a map to keep track of visited nodes
+        unordered_map<Node*, int> nodeMap;
+
+        // Traverse the linked list
+        while (temp != nullptr) {
+            // If node already exists in map, loop detected
+            if (nodeMap.find(temp) != nodeMap.end()) {
+                return true;
+            }
+            // Store the current node in the map
+            nodeMap[temp] = 1;
+
+            // Move to the next node
+            temp = temp->next;
+        }
+
+        // If traversal completes, no loop detected
+        return false;
+    }
