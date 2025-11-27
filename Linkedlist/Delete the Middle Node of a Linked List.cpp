c70e17380b9aba308796807c44ed416f6299a708
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-11-27 15:20:02 +0530

    Update Delete the Middle Node of a Linked List.cpp

--- Linkedlist/Delete the Middle Node of a Linked List.cpp
+++ Linkedlist/Delete the Middle Node of a Linked List.cpp
@@ -3,36 +3,36 @@
 //https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/
 
 // Broute force
- ListNode* findMid(ListNode* &head){
-    ListNode* slow=head;
-    ListNode* fast=head;
-    while(slow!=NULL && fast!=NULL){
-        fast=fast->next;
-        if(fast!=NULL){
-            slow=slow->next;
-            fast=fast->next;
-        }
-    }
-    return slow;
- }
-    ListNode* deleteMiddle(ListNode* head) {
-          if (head == NULL) return NULL;  // Handle the case of an empty list
-         if (head->next == NULL) {       // Handle the case of a single node
-           return NULL;
-    }
-        ListNode* mid=findMid(head);
-        ListNode* temp=head;
-       ListNode* prev=NULL;
-       while(temp!=NULL && temp!=mid){
-        prev=temp;
-        temp=temp->next;
-       }
-       if(prev!=NULL && temp!=NULL){
-        prev->next=temp->next;
-        delete temp;
-       }
-        
+ListNode* findmid(ListNode* &head){
+    ListNode* slow = head;
+    ListNode* fast = head;
 
-        return head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
     }
-};
+    return slow;       // middle node
+}
+
+ListNode* deleteMiddle(ListNode* head) {
+
+    // ⭐ Case 1: Only 1 node
+    if(head == NULL || head->next == NULL)
+        return NULL;
+
+    ListNode* mid = findmid(head);
+    ListNode* temp = head;
+    ListNode* prev = NULL;
+
+    // ⭐ Find the node just one before mid
+    while(temp != mid){
+        prev = temp;
+        temp = temp->next;
+    }
+
+    // ⭐ Delete middle
+    prev->next = temp->next;
+    delete temp;
+
+    return head;
+}
