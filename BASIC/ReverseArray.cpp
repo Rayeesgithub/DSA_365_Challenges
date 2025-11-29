7d9cc28213a08e1ff7e19706aed0efd90976f815
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-11-29 20:21:55 +0530

    Update ReverseArray.cpp

--- BASIC/ReverseArray.cpp
+++ BASIC/ReverseArray.cpp
@@ -1,11 +1,20 @@
+Input: N = 5, arr[] = {5,4,3,2,1}
+Output: {1,2,3,4,5}
+Explanation: Since the order of elements gets reversed the first element will occupy the fifth position, the second element occupies the fourth position and so on.
+
+// apprach1 Time=0(n) space=(1)
 #include<iostream>
 using namespace std;
-//by looping method
-// void reverseArray(int arr[],int n){
-//   for(int i=n-1;i>=0;i--){
-//     cout<<arr[i]<<" ";
-//   }
-  //by two pointer
+by looping method
+void reverseArray(int arr[],int n){
+  for(int i=n-1;i>=0;i--){
+    cout<<arr[i]<<" ";
+  }
+  
+
+
+// by two pointer
+Time=0(n) space=(1)
   void reverseArray(int arr[],int n){
     int i=0; int j=n-1;
     while(i<=j){
