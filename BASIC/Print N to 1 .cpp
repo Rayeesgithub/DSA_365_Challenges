94b346098c44e98263adfca0f4912cc5c1b20837
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-11-29 17:39:29 +0530

    Update Print N to 1 .cpp

--- BASIC/Print N to 1 .cpp
+++ BASIC/Print N to 1 .cpp
@@ -1,5 +1,10 @@
 //https://www.geeksforgeeks.org/problems/print-n-to-1-without-loop
 
+Input: N = 4
+Output: 1, 2, 3, 4
+Explanation: All the numbers from 1 to 4 are printed.
+
+ Time=0(n) && 
  void print(int N,int i){
           if(i<1) return;
           std::cout<<i<<" ";
