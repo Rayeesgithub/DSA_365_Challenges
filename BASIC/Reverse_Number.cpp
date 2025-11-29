3931e75db968c2b9a0e5923543f3feefad896e3f
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-11-29 15:13:10 +0530

    Update Reverse_Number.cpp

--- BASIC/Reverse_Number.cpp
+++ BASIC/Reverse_Number.cpp
@@ -1,18 +1,25 @@
-// leetcode problem
+Input: N = 12345
+Output:54321
+Explanation: The reverse of 12345 is 54321.
+
+  //Time-com=0(logn) spacecom=0(1)
+
+    
 class Solution {
 public:
-    int reverse(int x) {
+    int reverse(int n) {
    long long int ans=0;
-    while(x!=0) {
-       int digit=x%10;
-       if((ans>INT_MAX/10) || (ans<INT_MIN/10)){ //Handle overflow
-        return 0;
-       }
-        ans=ans*10+digit;
-        x=x/10;  
+   while(n != 0) {        // Loop until all digits are processed
+
+        int digit = n % 10;   // Extract last digit
+     
+       // Build reversed number
+        ans = ans * 10 + digit;
+       
+        n = n / 10;           // Remove last digit from n
     } 
           return ans;
     }
    
 };
-//Time-com=0(logn) spacecom=0(1)
+
