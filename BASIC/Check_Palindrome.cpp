9367410f37772a7503915423790fe7a3c9d87ae0
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-11-29 15:22:29 +0530

    Update Check_Palindrome.cpp

--- BASIC/Check_Palindrome.cpp
+++ BASIC/Check_Palindrome.cpp
@@ -1,23 +1,38 @@
 //https://leetcode.com/problems/palindrome-number/
-class Solution {
-public:
-    bool isPalindrome(int x) {
-        
-        int ans=0;
-        int original=x; // here you mistake
-        if(x<0) return false;
-        while(x>0){
-            int digit=x%10;
-            if((ans>INT_MAX/10)) return 0;
-            ans=ans*10+digit;
-            x=x/10;
-        }
-        if(ans==original){
-            return true;
-        }
-      else{
-        return false;
-      }
-      
+
+Example 1:
+Input:N = 4554
+Output:Palindrome Number
+Explanation: The reverse of 4554 is 4554 and therefore it is palindrome number
+
+
+Time-O(log₁₀(n)) &&
+Space=O(1)
+digit, ans, original → only few integer variables.No array or extra memory used.
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 45541;        // Input number
+    int original = n;     // Store original number to compare later
+    int ans = 0;          // This will store the reversed number
+
+    // Reverse the number
+    while(n != 0){
+        int digit = n % 10;      // Extract last digit
+        ans = ans * 10 + digit;  // Build reversed number
+        n = n / 10;              // Remove last digit from n
     }
-};
+
+    // Check if reversed number is equal to original number
+    if(ans == original){
+        cout << "this is Palindrome";
+    }
+    else{
+        cout << "Not palindrome";
+    }
+}
+
