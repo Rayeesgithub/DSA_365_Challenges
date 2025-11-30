3f36dbfbbf0239567974d4ba70774110dc8710cf
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-11-30 14:05:52 +0530

    Update Maximum_Minimum Fequency element.cpp

--- BASIC/Maximum_Minimum Fequency element.cpp
+++ BASIC/Maximum_Minimum Fequency element.cpp
@@ -1,3 +1,12 @@
+Example 1:
+Input: array[] = {10,5,10,15,10,5};
+Output: 10 15
+Explanation: The frequency of 10 is 3, i.e. the highest and the frequency of 15 is 1 i.e. the lowest.
+
+    
+
+    // Broute force
+    
 void countFreq(int arr[], int n)
 {
     vector<bool> visited(n, false);
@@ -30,7 +39,13 @@ void countFreq(int arr[], int n)
     cout << "The highest frequency element is: " << maxEle << "\n";
     cout << "The lowest frequency element is: " << minEle << "\n";
 }
+
+
+
 //second approach
+Total Average Complexity = O(n) && space= O(n)
+
+    
 void Frequency(int arr[], int n)
 {
     unordered_map<int, int> map;
