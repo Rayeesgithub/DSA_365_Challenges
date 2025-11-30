d7bb5d477684e4d1fe3693787a1d4c28678bc4e0
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-11-30 15:54:24 +0530

    Update Fraction_of_Two_Number.cpp

--- BASIC/Fraction_of_Two_Number.cpp
+++ BASIC/Fraction_of_Two_Number.cpp
@@ -1,3 +1,8 @@
+Input: Numerator1 = 3, Denominator1 = 4, Numerator2 = 1, Denominator2 = 7
+Output: Numerator = 25, Denominator = 28 
+Explanation: 3/4 + 1/7 = 25/28
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int findGCD(int n,int m){
@@ -9,14 +14,27 @@ int findGCD(int n,int m){
   }
   return ans;
 }
-void solveFraction(int num1,int den1,int num2,int den2){
-  int numerator=num1*den2+num2*den1;
-   int denometr=den1*den2;
-   int gcd=findGCD(numerator,denometr);
-   numerator=numerator/gcd;
-   denometr=denometr/gcd;
-   cout<<numerator<<"/"<<denometr;
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n1 = 3, d1 = 4;   // Fraction 1 = 3/4
+    int n2 = 1, d2 = 7;   // Fraction 2 = 1/7
+
+    // Step 1: LCM of denominators
+    int lcm = (d1 * d2) / FindGCD(d1, d2);
+
+    // Step 2: Convert both numerators to same denominator
+    int num1 = n1 * (lcm / d1);
+    int num2 = n2 * (lcm / d2);
+
+    // Step 3: Add numerators
+    int final_num = num1 + num2;
+    int final_den = lcm;
+
+    cout << "Numerator = " << final_num << ", Denominator = " << final_den;
 }
+
 int main(){
   int t;
   cin>>t;
