b6b16c6550aafff9f1c5c080479b3ca17ded4c7d
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-12-04 13:29:15 +0530

    Update MajorityElemnt_three.cpp

--- Array/MajorityElemnt_three.cpp
+++ Array/MajorityElemnt_three.cpp
@@ -1,87 +1,72 @@
 https://leetcode.com/problems/majority-element-ii/
-//striver sheet
-class Solution {
-public:
-    vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size(); // Get the size of the input vector
-        vector<int> ans; // List to store the majority elements
+Example 1:  
+Input: nums = [1, 2, 1, 1, 3, 2]  
+Output: [1]  
+Explanation: Here, n / 3 = 6 / 3 = 2.  
+Therefore, the elements appearing 3 or more times are: [1].
 
-        // Approach 1: Brute Force
-        for (int i = 0; i < n; i++) {
-            // Check if nums[i] is not already a part of the answer
-            if (ans.size() == 0 || ans[0] != nums[i]) {
-                int cnt = 0; // Counter to count the frequency of nums[i]
-                for (int j = 0; j < n; j++) {
-                    // Counting the frequency of nums[i]
-                    if (nums[j] == nums[i]) {
-                        cnt++;
-                    }
+
+
+
+
+    
+
+// Approach 1: Brute Force Time = O(n^2), Space = O(1)
+
+vector<int> majorityElement(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> ans;     // To store result (max 2 elements possible for n/3 rule)
+
+    // ----------------------------
+    // TRY every element one by one
+    // ----------------------------
+    for (int i = 0; i < n; i++) {
+
+        // To avoid counting same number again
+        // If ans empty OR nums[i] is NOT equal to first stored element, then check
+        if (ans.size() == 0 || ans[0] != nums[i]) {
+            
+            int cnt = 0;   // count frequency of nums[i]
+
+            // Count how many times nums[i] appears
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == nums[i]) {
+                    cnt++;
                 }
-
-                // Check if the frequency is greater than n/3
-                if (cnt > (n / 3))
-                    ans.push_back(nums[i]); // Add nums[i] to the answer list
             }
 
-            // If we have found two majority elements, break
-            if (ans.size() == 2) break;
+            // Check if frequency > n/3
+            if (cnt > (n / 3))
+                ans.push_back(nums[i]);    // Add it to answer
         }
 
-        // Approach 2: Using Hashmap
-      vector<int> Majority(vector<int>&arr,int n){
-    unordered_map<int,int>mp; vector<int>ans;
-    for(int i=0; i<n; i++){
-        mp[arr[i]]++;
+        // Maximum possible majority elements = 2
+        if (ans.size() == 2) break;
     }
 
-    for(auto it:mp){
-        if(it.second>n/3){
-            ans.push_back(it.first);
-        }
-    }
     return ans;
 }
 
-        // Approach 3: Extended Boyer-Moore Voting Algorithm
-        int cnt1 = 0, cnt2 = 0; // Counters for the two majority elements
-        int el1 = INT_MIN; // Element 1
-        int el2 = INT_MIN; // Element 2
+       
+✅ Approach 2: Hashmap (Time = O(n), Space = O(n))
+// Approach 2: Using Hashmap
 
-        // Applying the Boyer-Moore Voting Algorithm
-        for (int i = 0; i < n; i++) {
-            if (cnt1 == 0 && el2 != nums[i]) {
-                cnt1 = 1;
-                el1 = nums[i]; // Update element 1
-            }
-            else if (cnt2 == 0 && el1 != nums[i]) {
-                cnt2 = 1;
-                el2 = nums[i]; // Update element 2
-            }
-            else if (nums[i] == el1) {
-                cnt1++; // Increment the counter for element 1
-            }
-            else if (nums[i] == el2) {
-                cnt2++; // Increment the counter for element 2
-            }
-            else {
-                cnt1--; // Decrement both counters
-                cnt2--;
-            }
-        }
+vector<int> Majority(vector<int>& arr, int n) {
 
-        vector<int> ls; // List to store the final majority elements
+    unordered_map<int, int> mp;     // Stores frequency of each number
+    vector<int> ans;                // To store final results
 
-        // Manually check if the stored elements in el1 and el2 are the majority elements
-        cnt1 = 0, cnt2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == el1) cnt1++; // Count the occurrences of el1
-            if (nums[i] == el2) cnt2++; // Count the occurrences of el2
-        }
-
-        int mini = int(n / 3) + 1; // Minimum count required to be a majority element
-        if (cnt1 >= mini) ls.push_back(el1); // Add el1 to the list if it is a majority element
-        if (cnt2 >= mini) ls.push_back(el2); // Add el2 to the list if it is a majority element
-        
-        return ls; // Return the list of majority elements
+    // Count frequency of every element
+    for (int i = 0; i < n; i++) {
+        mp[arr[i]]++;
     }
-};
+
+    // Check which elements occur more than n/3 times
+    for (auto it : mp) {
+        if (it.second > n / 3) {
+            ans.push_back(it.first);
+        }
+    }
+
+    return ans;
+}
