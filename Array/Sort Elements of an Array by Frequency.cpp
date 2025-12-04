384e0c39ee996a2cc289372d4ef1b534fc4c645b
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-12-04 20:06:54 +0530

    Update Sort Elements of an Array by Frequency.cpp

--- Array/Sort Elements of an Array by Frequency.cpp
+++ Array/Sort Elements of an Array by Frequency.cpp
@@ -1,5 +1,11 @@
 StriverNqt
+Input: N = 8, array[] = {1,2,3,2,4,3,1,2}
+Output: 2 2 2 1 1 3 3 4 
+Explanation: Since  2 is present 3 times in an array , so print it 3 times 
+    ,then print ‘1’ 2 times and then ‘3’ 2 times and 4 has least frequency, it will be printed at last.
 
+
+    
 #include<bits/stdc++.h>
 using namespace std;
 bool compare(pair<int, int>& a, pair<int, int>& b) {
