ed72967cee7856270447b8d934cab080586edee4
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-12-04 18:08:32 +0530

    Update Rearrange array in increasing-decreasing order.cpp

--- Array/Rearrange array in increasing-decreasing order.cpp
+++ Array/Rearrange array in increasing-decreasing order.cpp
@@ -1,22 +1,35 @@
 // TCS NQT Striver
-#include<bits/stdc++.h>
+Input: [8 7 1 6 5 9]
+Output: [1 5 6 9 8 7]
+Explanation: First three elements are in the ascending order and next three elements are in the descending order.
+
+
+Time=O(n log n) && space=0(1)
+
+   #include<bits/stdc++.h>
 using namespace std;
 
-
-
 int main() {
-   int n=6;
-   int arr[n]={8,7,1,6,5,9};
-   int mid=n/2;
-   vector<int>ans;
-   sort(arr,arr+n);
-   for(int i=0; i<mid; i++){
-    ans.push_back(arr[i]);
-   }
-   for(int i=n-1; i>=mid; i--){
-    ans.push_back(arr[i]);
-   }
-   for(int i=0; i<ans.size(); i++){
-    cout<<ans[i]<<" ";
-   }
+
+    vector<int> arr = {8, 7, 1, 2, 6, 5, 9};
+
+    // Sort the array in increasing order
+    // Sorting takes O(n log n) time
+    sort(arr.begin(), arr.end());
+
+    // -------------------------------------------
+    // Print the first half of the sorted array 
+    // (from smallest elements)
+    // -------------------------------------------
+    for (int i = 0; i < arr.size() / 2; i++) {
+        cout << arr[i] << " ";
+    }
+
+    // -------------------------------------------
+    // Print the second half of the array 
+    // but in reverse (from largest elements)
+    // -------------------------------------------
+    for (int i = arr.size() - 1; i >= arr.size() / 2; i--) {
+        cout << arr[i] << " ";
+    }
 }
