3adc23b50b233e6021f29105f29a964e16eec146
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-12-09 18:25:54 +0530

    Update Merge Two Sorted Array.cpp

--- Array/Merge Two Sorted Array.cpp
+++ Array/Merge Two Sorted Array.cpp
@@ -1,3 +1,9 @@
+Input : nums1 = [-5, -2, 4, 5, 0, 0, 0], nums2 = [-3, 1, 8]
+Output : [-5, -3, -2, 1, 4, 5, 8]
+Explanation : The merged array is: [-5, -3, -2, 1, 4, 5, 8], where [-5, -2, 4, 5] are from nums1 and [-3, 1, 8] are from nums2
+
+
+
 
 
 //approach1->Borute force
@@ -19,31 +25,28 @@ void Merg(vector<int>&arr1,vector<int>&arr2){
   }
 
 // approach2-> Optimal Approach
-T.C-0(n) S.C->0(n)
-void MergeArr(vector<int>&arr1,vector<int>&arr2){
-    int n=arr1.size(); int m=arr2.size();
-    vector<int>ans;
-    int i=0; int j=0;
-    while(i<n && j<m){
-      if(arr1[i]<arr2[j]){
-        ans.push_back(arr1[i]);
-        i++;
-      }
-      else{
-        ans.push_back(arr2[j]);
-        j++;
-      }
-    }
-    while(i<n){
-      ans.push_back(arr1[i]);
-      i++;
-    }
-    while(j<m){
-      ans.push_back(arr2[j]);
-      j++;
+T.C-0(n+m) S.C->0(1)
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i = m - 1;        // pointer for nums1 last element
+    int j = n - 1;        // pointer for nums2 last element
+    int k = m + n - 1;    // pointer for nums1 final position
+
+    // Compare from the end and place the larger element at the end of nums1
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
     }
 
-    for(int i=0; i<ans.size(); i++){
-    cout<<ans[i]<<" ";
+    // If nums2 still has elements left, copy them
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
     }
 }
