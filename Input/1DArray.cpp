303aee4a34b8e2bc8374d67170afbb35f42a9dca
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2026-03-25 16:55:32 +0530

    Update 1DArray.cpp

--- Input/1DArray.cpp
+++ Input/1DArray.cpp
@@ -46,4 +46,128 @@ while(cin >> n >> c){
 }
 
 
-#### 
+#### 🔥 3. String Format Array Input
+👉 Input: 1 2 3 4 5
+    string s;
+getline(cin, s);
+
+stringstream ss(s);
+int x;
+
+while(ss >> x) {
+    cout << x << " ";
+}
+
+Example-> input Format  —  "1 2 3 4 5"  (whole line as string)
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string line;
+    getline(cin, line);       // reads: "1 2 3 4 5"
+
+   stringstream ss(line);   // wrap string as stream
+    vector<int> arr;
+    int num;
+
+    while(ss >> num)          // extract ints one by one
+        arr.push_back(num);
+
+    for(int x : arr) cout << x << " ";
+}
+
+HOW it works
+1
+getline(cin, line) reads the entire line including spaces into a string.
+2
+istringstream ss(line) treats that string exactly like a cin stream.
+3 . ss >> num extracts integers one by one, skipping spaces automaticall
+
+input format
+5
+10 20 30 40 50
+code
+int n; cin >> n;
+cin.ignore();           // clears leftover '\n' — REQUIRED
+string line;
+getline(cin, line);
+
+istringstream ss(line);
+vector<int> arr;
+int num;
+while(ss >> num) arr.push_back(num);
+Always add cin.ignore() after cin>>n before getline — without it getline reads an empty line!
+
+
+
+
+##### 🔥 2) Comma Separated Array
+👉 Input: 1,2,3,4,5
+
+    #include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    vector<int> arr;
+    string temp;
+
+    stringstream ss(s);
+    while(getline(ss, temp, ',')) {
+        arr.push_back(stoi(temp));
+    }
+
+    for(int i : arr) cout << i << " ";
+}
+
+
+
+
+##### 🔥 3) Bracket 
+👉  Input Format  —  [1,2,3,4,5]  or  {1,2,3,4,5}
+
+    #include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string line;
+    getline(cin, line);   // reads "[1,2,3,4,5]"
+
+    // replace brackets AND commas with space
+    for(char& c : line)
+        if(c=='['||c==']'||c=='{'||c=='}'||c==',')
+            c = ' ';
+    // line is now: "  1 2 3 4 5  "
+
+    istringstream ss(line);
+    vector<int> arr;
+    int num;
+    while(ss >> num)      // handles multiple spaces fine
+        arr.push_back(num);
+
+    for(int x : arr) cout << x << " ";
+}
+
+
+
+#### 🔥 4) String of Space Separated Words
+
+👉 Input: hello world from tcs
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    stringstream ss(s);
+    string word;
+
+    vector<string> words;
+    while(ss >> word) {
+        words.push_back(word);
+    }
+
+    for(string w : words) cout << w << endl;
+}
