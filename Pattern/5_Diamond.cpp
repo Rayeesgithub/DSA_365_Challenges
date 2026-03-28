ae8e054f1e0da00644072ff2d8f3b274d5a2fe36
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2026-03-28 10:17:57 +0530

    Update 5_Diamond.cpp

--- Pattern/5_Diamond.cpp
+++ Pattern/5_Diamond.cpp
@@ -51,8 +51,83 @@ int main() {
 
 
 
-### Full Diamond Shape
+### 2. Full Diamond Shape
 
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+    // 1st approach
+
+int main(){
+  int n=5;
+  for(int i=1; i<=n; i++){
+    for(int j=1; j<=n-i; j++){
+      cout<<" ";
+    }
+
+    for(int k=1; k<=2*i-1; k++){
+      cout<<"*";
+    }
+    cout<<endl;
+  }
+  for(int i=1; i<=n; i++){
+    for(int j=1; j<=i; j++){
+      cout<<" ";
+    }
+
+    for(int k=1; k<=2*(n-i)-1; k++){
+      cout<<"*";
+    }
+    cout<<endl;
+  }
+   
+}
+
+
+// 2nd approach
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    // Upper pyramid
+    for(int i = 1; i <= n; i++) {
+
+        // spaces
+        for(int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // stars
+        for(int j = 1; j <= 2*i - 1; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // Lower pyramid
+    for(int i = n - 1; i >= 1; i--) {
+
+        // spaces
+        for(int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // stars
+        for(int j = 1; j <= 2*i - 1; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
     
     
     
