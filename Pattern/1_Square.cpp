5eb407910f281b7386b122f5b702216640abf06b
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2026-05-09 14:50:21 +0530

    Update 1_Square.cpp

--- Pattern/1_Square.cpp
+++ Pattern/1_Square.cpp
@@ -21,3 +21,36 @@ int main() {
 ```
 
 
+#### Hollow Rectangle Pattern
+
+*****
+*   *
+*   *
+*****
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int rows = 4;
+    int cols = 5;
+
+    for(int i=1; i<=rows; i++){
+
+        for(int j=1; j<=cols; j++){
+
+            // border condition
+            if(i==1 || i==rows || j==1 || j==cols){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
