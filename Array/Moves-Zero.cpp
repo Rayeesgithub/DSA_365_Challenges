ddf3cc4cbf69ea52416fbcb3684bcc215d9816e7
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2026-05-17 08:24:02 +0530

    Update Moves-Zero.cpp

--- Array/Moves-Zero.cpp
+++ Array/Moves-Zero.cpp
@@ -42,56 +42,18 @@ void moveZeroes(vector<int>& arr) {
 // broute force-> approach-2
 T.Complexity=0(n) && space complexity=0(n)
 
-    
-void moveZeroes(vector<int>& nums) {
-
-    int n = nums.size();
-    vector<int> ans;   // Stores non-zero elements
-    vector<int> ans2;  // Stores zero elements
-
-    // STEP 1: Separate non-zeros and zeros into 2 arrays
-    for (int i = 0; i < n; i++) {
-
-        if (nums[i] != 0) {
-            ans.push_back(nums[i]);   // non-zero
-        }
-        else {
-            ans2.push_back(nums[i]);  // zero
-        }
-    }
-
-    // STEP 2: Place all non-zeros back in nums[]
-    for (int i = 0; i < ans.size(); i++) {
-        nums[i] = ans[i];
-    }
-
-    // STEP 3: Place all zeros at the end
-    int m = ans.size();
-    for (int i = 0; i < ans2.size(); i++) {
-        nums[m + i] = ans2[i];
-    }
-}
-
-
-
 
 // otimize approazed
-T.Complexity=0(n) && space complexity=0(1)
- void MoveZero(int arr[], int &n) {
+// PATTERN-> Same Direction Template(Two Pointer)
 
-    int left = 0;  // Pointer for placing next non-zero
-
-    // right pointer moves normally
-    for (int right = 0; right < n; right++) {
-
-        // Whenever a non-zero is found
-        if (arr[right] != 0) {
-
-            // Swap it with element at 'left'
-            swap(arr[left], arr[right]);
-
-            // Move left to next position
-            left++;
+// T.Complexity=0(n) && space complexity=0(1)
+void moveZeroes(vector<int>& arr) {
+    int slow = 0;
+    
+    for(int fast = 0; fast < arr.size(); fast++) {
+        if(arr[fast] != 0) {
+            swap(arr[slow], arr[fast]);
+            slow++;
         }
     }
 }
